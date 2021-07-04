18aa37708d743f91dfaa8d2152e301b80d895cff
huangjianbo <baqimeilaoban@gmail.com>
2021-07-04 18:37:06 +0800

    0704 study01

---
+++ day04/03struct_demo1/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+//结构体
+
+type person struct {
+	name   string
+	age    int
+	gender string
+	hobby  []string
+}
+
+func main() {
+	//声明一个person类型的变量p
+	var p person
+	//通过字段去赋值
+	p.name = "周林"
+	p.age = 9000
+	p.gender = "男"
+	p.hobby = []string{"篮球", "足球", "双色球"}
+	fmt.Println(p)
+	fmt.Printf("%T\n", p)
+	fmt.Println(p.name)
+	var p2 person
+	p2.name = "理想"
+	p2.age = 18
+	fmt.Printf("%T\n", p2)
+	fmt.Println(p2)
+}
